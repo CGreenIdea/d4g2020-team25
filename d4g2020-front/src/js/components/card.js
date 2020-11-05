512afd2bd512181c3bcb0b10818c8d6e2cb36dbb
Fabien H <fabien.hanesse@outlook.com>
2020-11-05 22:17:40 +0100

    Started implementation for the city data display

--- d4g2020-front/src/js/components/card.js
+++ d4g2020-front/src/js/components/card.js
@@ -1,14 +1,40 @@
-function showCard() {
-    fillCityData();
-    fillDepartmentData();
-    fillRegionData();
+function showCard(data) {
+    if (data != null) {
+        if (data.city != null) {
+            fillCityData();
+        }
+
+        if (data.department != null) {
+            fillDepartmentData();
+        }
+
+        if (data.region != null) {
+            fillRegionData();
+        }
+    }
 }
 
-function fillCityData() {}
+function fillCityData(cityData) {
+    // Scoring in header
+    document.getElementById("headerScore").innerHTML(cityData.scoring);
 
-function fillDepartmentData() {}
+    // Scoring in the detail
+    document.getElementById("cityScore").innerHTML(cityData.scoring);
 
-function fillRegionData() {}
+    let detailContent = ""; //getValueRow(cityData.criteriaLabel, cityData.criteriaValue, null);
+
+    //detailContent += getValueRow(cityData.criteriaLabel, cityData.criteriaValue, null);
+
+    document.querySelector("#cityData .content").innerHTML(detailContent);
+}
+
+function fillDepartmentData(departmentData) {
+
+}
+
+function fillRegionData(regionData) {
+
+}
 
 function getValueRow(label, value, suffix) {
     return `<div class="content-property-name">${label}</div><div class="content-property-value">${value} ${suffix}</div>`;
