985bccf0e69fc5ea037d102140634c8e6b61b1f0
Cyrille Chopelet <cyrille.chopelet@mines-nancy.org>
2020-11-06 01:25:38 +0100

    [Sonar] Clean code

--- d4g2020-front/src/js/index.js
+++ d4g2020-front/src/js/index.js
@@ -1,7 +1,7 @@
-import initSearchForm from './business/search-form'; './business/search-form';
+import initSearchForm from './business/search-form';
 
 import { initCollapsables } from './components/collapsable.js';
-import { showCard, hideCard } from './components/card.js';
+//import { showCard, hideCard } from './components/card.js';
 
 initSearchForm();
 initCollapsables();
